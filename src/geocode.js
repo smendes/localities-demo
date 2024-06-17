a47c358e5c0120d4c20eeb1fa0e2a8fe1cf7b0eb
Leo PERNELLE (Woosmap) <lpernelle@webgeoservices.com>
2024-06-17 13:56:16 +0200

    Update geocode.js

--- src/geocode.js
+++ src/geocode.js
@@ -1,1 +1,37 @@
+import { getTargetEnvironment } from "./environment_select.js";
+const lang = "fr";
+export function localitiesReverseGeocode(
+  latlng,
+  components,
+  types
+) {
+  const env = getTargetEnvironment();
+  const args = {
+    key: env.woosmap_key,
+    latlng,
+  };
+  if (components !== "") {
+    args.components = components;
+  }
+  if (types !== "") {
+    args.types = types;
+  }
 
+  return fetch(`${env.url}geocode/?${buildQueryString(args)}`).then(
+    (response) => response.json()
+  );
+}
+
+export function buildQueryString(params) {
+  const queryStringParts = [];
+
+  for (let key in params) {
+    if (params.hasOwnProperty(key)) {
+      let value = params[key];
+      queryStringParts.push(
+        `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+      );
+    }
+  }
+  return queryStringParts.join("&");
+}
