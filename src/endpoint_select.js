6d87e4d21881b97c19da5af9e508dcbac6ef6db5
Leo PERNELLE (Woosmap) <lpernelle@webgeoservices.com>
2025-04-14 14:50:44 +0200

    Update endpoint_select.js

--- src/endpoint_select.js
+++ src/endpoint_select.js
@@ -1,1 +1,13 @@
 // endpoint_select.js
+
+export function getTargetEnpoint() {
+  var selectedEnpoint = document.getElementById("endpoint-select").value;
+  console.log(
+    `**endpoint: ${selectedEnpoint.toUpperCase()} **`
+  );
+  return selectedEnpoint;
+}
+
+document.getElementById("endpoint-select").addEventListener("change", (e) => {
+  // nothing
+});
